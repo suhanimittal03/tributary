2c5c1ce0db68b4e5810d2d58ea11bc8aebd017cc
Suhani Mittal <suhani.mittal@iitgn.ac.in>
2024-05-22 16:35:21 +0530

    Updated Dockerfile to bind gunicorn to 0.0.0.0:8000 and modified entrypoint.py to implement engine temperature recording and collection with Redis

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.10
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
+
+COPY ./test_file.py .
 COPY ./entrypoint.py .
 # CMD echo "Hello World"
 # CMD python entrypoint.py
-CMD exec gunicorn entrypoint:app
+CMD exec gunicorn --bind 0.0.0.0:8000 entrypoint:app
--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+import json
+import redis as redis
+
+HISTORY_LENGTH = 10
+DATA_KEY = 'engine_temperature'
+
+
 app = Flask(__name__)
 
 @app.route('/record', methods=['POST'])
-
 def record_engine_temperature():
-    # pass
+    payload = request.get_json(force=True)
+    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
+
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
+
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database.lpush(DATA_KEY, engine_temperature)
+    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
+
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine_temperature list now contains these values: {engine_temperature_values}")
+
+    logger.info(f"record request successful")
     return {"success": True}, 200
 
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
     return {"success": True}, 200
+    
